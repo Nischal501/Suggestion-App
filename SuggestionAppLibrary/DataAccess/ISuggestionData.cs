770caaf067c45373eb0ead498eb0174d519c06a9
Nischal501 <99686312+Nischal501@users.noreply.github.com>
2023-04-19 12:56:08 -0600

    Add files via upload

---
+++ SuggestionAppLibrary/DataAccess/ISuggestionData.cs
@@ -0,0 +1,14 @@
+﻿namespace SuggestionAppLibrary.DataAccess
+{
+    public interface ISuggestionData
+    {
+        Task CreateSuggestion(SuggestionModel suggestion);
+        Task<List<SuggestionModel>> GetAllApprovedSuggestions();
+        Task<List<SuggestionModel>> GetAllSuggestions();
+        Task<List<SuggestionModel>> GetAllSuggestionswaitingForApproval();
+        Task<SuggestionModel> GetSuggestion(string id);
+        Task<List<SuggestionModel>> GetUsersSuggestions(string userId);
+        Task UpdateSuggestion(SuggestionModel suggestion);
+        Task UpvoteSuggestion(string suggestionId, string userId);
+    }
+}
